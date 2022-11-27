0e86429eb4acd77ff56091e6ef3c0450c0cf3a46
Devika Gosala <devikag621@gmail.com>
2022-11-27 13:14:48 +0530

    Demo for Team

---
+++ src/main/java/com/app/demo/NewProject/App.java
@@ -0,0 +1,16 @@
+package com.app.demo.NewProject;
+
+/**
+ * 
+ * Hello World!
+ *
+ */
+public class App {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello World!");
+
+	}
+
+}
